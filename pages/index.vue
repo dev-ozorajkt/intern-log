1f8ad145fd3544056d12ac6f3b3e6e24d0c7bb67
ozorajkt <intern.ozorajkt@gmail.com>
2022-09-12 17:50:01 +0700

    chore: ui fix

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,19 @@
 <template>
-  <div class="bg-index min-h-full py-10">
-  <Head>
-    <Title>Login</Title>
-  </Head>
-  <!-- <div v-if="user">
-      Welcome {{ user?.email }}
-  </div>   -->
-  <div class="max-w-md mx-auto p-5 bg-gray-200/20 dark:bg-gray-700/70 rounded backdrop-blur-sm drop-shadow-lg" >    
-    <Login />
+  <div>
+    <NuxtLayout name="login">        
+    <div class="bg-index min-h-full py-10">
+      <Head>
+        <Title>Login</Title>
+      </Head>
+      <!-- <div v-if="user">
+          Welcome {{ user?.email }}
+      </div>   -->
+      <div class="max-w-md mx-auto p-5 bg-gray-200/20 dark:bg-gray-700/70 rounded backdrop-blur-sm drop-shadow-lg" >    
+        <Login />
+      </div>
+    </div>  
+    </NuxtLayout>
   </div>
-  </div>  
 </template>
 
 <script setup lang="ts">
@@ -17,7 +21,7 @@
   const client = useSupabaseClient()
   const user = useSupabaseUser()
   definePageMeta({
-    layout: "login",
+    layout: false,
   });
   watchEffect(() => {
     if (user.value) {
